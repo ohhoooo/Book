be05353bfc6434de8d9c6de484d65724719d0f48
KJH <gh7052@gmail.com>
2024-05-01 17:52:33 +0900

    Add: NetworkManager 추가

---
+++ Book/Model/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Book
+//
+//  Created by 김정호 on 5/1/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    // MARK: - properties
+    
+    // MARK: - methods
+}
