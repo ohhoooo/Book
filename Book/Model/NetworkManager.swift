12736c37597c133d50945479c05fc53ae2efead6
KJH <gh7052@gmail.com>
2024-05-02 18:37:15 +0900

    feat: NetworkManager 책 리스트 가져오기 기능 추가

--- Book/Model/NetworkManager.swift
+++ Book/Model/NetworkManager.swift
@@ -7,9 +7,52 @@
 
 import Foundation
 
+enum Sort {
+    case accuracy
+    case latest
+}
+
 final class NetworkManager {
     
     // MARK: - properties
     
+    // singleton
+    static let shared = NetworkManager()
+    private init() {}
+    
+    // base
+    private let basedUrl = "https://dapi.kakao.com/v3/search/book"
+    
+    // header
+    private let authorization = Bundle.main.infoDictionary?["APIKey"] as! String
+    
     // MARK: - methods
+    func fetchBooks(query: String, sort: Sort, page: Int, completion: @escaping ((Result<Response, Error>) -> Void)) {
+        guard let url = URL(string: "\(basedUrl)?query=\(query)&sort=\(Sort.latest)&page=\(page)") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue(authorization, forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                print(error)
+                return
+            }
+            
+            guard let data else {
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(Response.self, from: data)
+                completion(.success(response))
+            } catch {
+                return
+            }
+        }
+        
+        task.resume()
+    }
 }
