35d0513707a3278e6f7f833d72db24e5f201bcc4
KJH <gh7052@gmail.com>
2024-05-05 23:53:12 +0900

    Design: SearchView UI 구성

--- Book/View/SearchView.swift
+++ Book/View/SearchView.swift
@@ -6,17 +6,78 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchView: UIView {
 
     // MARK: - properties
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "원하는 책을 입력하세요"
+        searchBar.backgroundColor = .white
+        return searchBar
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
     
     // MARK: - methods
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        configureUI()
+        configureConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func configureConstraints() {
+        self.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.top.bottom.equalTo(self.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { [self] sectionIndex, _ in
+            switch sectionIndex {
+            default:
+                // item
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0/3.0), heightDimension: .fractionalHeight(1.0))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+                
+                // group
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                
+                // section
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [createHeader()]
+                
+                return section
+            }
+        }
+    }
+    
+    private func createHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(57.0)),
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        
+        return header
     }
 }
