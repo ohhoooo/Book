18079274306733d0690a67b72c71120e0f15a7d1
KJH <gh7052@gmail.com>
2024-05-07 00:13:53 +0900

    Design: SearchResultBookHeaderView UI 구성

--- Book/View/SearchResultBookHeaderView.swift
+++ Book/View/SearchResultBookHeaderView.swift
@@ -6,12 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchResultBookHeaderView: UICollectionReusableView {
 
     // MARK: - properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22, weight: .heavy)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 1
+        return label
+    }()
     
     // MARK: - life cycles
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.prepare(text: nil)
+    }
     
     // MARK: - methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func configureConstraint() {
+        self.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+    func prepare(text: String?) {
+        self.titleLabel.text = text
+    }
 }
