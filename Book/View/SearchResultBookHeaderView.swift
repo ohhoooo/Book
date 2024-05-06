69736065c6e7b4c9ec8e835f99abe6a12a8625e3
KJH <gh7052@gmail.com>
2024-05-06 23:55:23 +0900

    Add: SearchResultBookHeaderView 추가

---
+++ Book/View/SearchResultBookHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultBookHeaderView.swift
+//  Book
+//
+//  Created by 김정호 on 5/6/24.
+//
+
+import UIKit
+
+final class SearchResultBookHeaderView: UICollectionReusableView {
+
+    // MARK: - properties
+    
+    // MARK: - life cycles
+    
+    // MARK: - methods
+}
