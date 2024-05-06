194e54a7cca5aa35be45aa76f2e089446a0c8ab6
KJH <gh7052@gmail.com>
2024-05-06 23:40:40 +0900

    Feat: SearchViewController configureCollectionView 메소드 추가

--- Book/Controller/SearchViewController.swift
+++ Book/Controller/SearchViewController.swift
@@ -13,6 +13,8 @@ final class SearchViewController: UIViewController {
     private let searchView = SearchView()
     
     private let networkManager = NetworkManager.shared
+    
+    private var query = ""
     private var sort = Sort.accuracy
     private var page = 1
     private var isEnd = false
@@ -28,6 +30,7 @@ final class SearchViewController: UIViewController {
         super.viewDidLoad()
         
         configureNavigationBar()
+        configureCollectionView()
     }
     
     // MARK: - methods
@@ -35,7 +38,14 @@ final class SearchViewController: UIViewController {
         self.navigationItem.titleView = searchView.searchBar
     }
     
-    private func fetchSearchResultBooks(query: String) {
+    private func configureCollectionView() {
+        self.searchView.collectionView.delegate = self
+        self.searchView.collectionView.dataSource = self
+        
+        self.searchView.collectionView.register(SearchResultBookCell.self, forCellWithReuseIdentifier: "SearchResultBookCell")
+    }
+    
+    private func fetchSearchResultBooks() {
         if isEnd {
             return
         }
@@ -43,6 +53,7 @@ final class SearchViewController: UIViewController {
         networkManager.fetchBooks(query: query, sort: sort, page: page) { [weak self] result in
             switch result {
             case .success(let response):
+                self?.page += 1
                 self?.isEnd = response.meta.isEnd
                 self?.searchResultBooks += response.books
                 
@@ -53,7 +64,33 @@ final class SearchViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == searchResultBooks.count - 1 && isEnd == false {
+            fetchSearchResultBooks()
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searchResultBooks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchResultBookCell", for: indexPath) as? SearchResultBookCell else {
+            return UICollectionViewCell()
+        }
         
-        page += 1
+        cell.bind(book: searchResultBooks[indexPath.row])
+        
+        return cell
     }
 }
