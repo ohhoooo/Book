eb29cfe686706980add4faa8ed80c421c2787357
KJH <gh7052@gmail.com>
2024-05-06 19:36:57 +0900

    Add: SearchResultBookCell 추가

---
+++ Book/View/Cell/SearchResultBookCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultBookCell.swift
+//  Book
+//
+//  Created by 김정호 on 5/6/24.
+//
+
+import UIKit
+
+final class SearchResultBookCell: UICollectionViewCell {
+    
+    // MARK: - properties
+    
+    // MARK: - life cycles
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    // MARK: - methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
