3f694c0faa7e389889bc36b1d8cc3b41c4f97f9d
KJH <gh7052@gmail.com>
2024-05-07 20:10:31 +0900

    Feat: BookDetailViewController 그랩 바 기능 추가

--- Book/Controller/BookDetailViewController.swift
+++ Book/Controller/BookDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class BookDetailViewController: UIViewController {
-
+    
     // MARK: - properties
     private let bookDetailView = BookDetailView()
     
@@ -19,7 +19,19 @@ final class BookDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
     }
     
     // MARK: - methods
+    private func configureUI() {
+        if let sheetPresentationController = sheetPresentationController {
+            sheetPresentationController.detents = [ .large()]
+        }
+        sheetPresentationController?.prefersGrabberVisible = true
+    }
+    
+    func bind(book: Book) {
+        self.bookDetailView.bind(book: book)
+    }
 }
