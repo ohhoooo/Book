038c506c92cd2ca9a48ebff93e2f42ff6fe6f02e
KJH <gh7052@gmail.com>
2024-05-07 15:25:00 +0900

    Feat: String+Extension setLineSpacing 메소드 추가

--- Book/Extension/String+Extension.swift
+++ Book/Extension/String+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by 김정호 on 5/7/24.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    func setLineSpacing(_ spacing: CGFloat) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = spacing
+        
+        return NSAttributedString(
+            string: self,
+            attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle]
+        )
+    }
+}
