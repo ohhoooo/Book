35710e79e9fef31581ba588ef6c45991879ddee9
KJH <gh7052@gmail.com>
2024-05-08 17:06:22 +0900

    Add: CoreDataSavedBookStorage 추가

---
+++ Book/Model/PersistentStorage/SavedBookStorage/CoreDataSavedBookStorage.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataSavedBookStorage.swift
+//  Book
+//
+//  Created by 김정호 on 5/8/24.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataSavedBookStorage {
+    
+    // MARK: - properties
+    
+    // MARK: - methods
+}
