fbd17d7be8ca47cc91090df68f5e3bab0d8ac327
KJH <gh7052@gmail.com>
2024-05-10 05:12:00 +0900

    Design: RecentViewedBookCell UI 구성

--- Book/View/Cell/RecentViewedBookCell.swift
+++ Book/View/Cell/RecentViewedBookCell.swift
@@ -6,7 +6,56 @@
 //
 
 import UIKit
+import SnapKit
 
-class RecentViewedBookCell: UICollectionViewCell {
+final class RecentViewedBookCell: UICollectionViewCell {
     
+    // MARK: - properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        label.backgroundColor = UIColor(red: 107/255, green: 184/255, blue: 100/255, alpha: 1)
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 12
+        return label
+    }()
+    
+    // MARK: - life cycles
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = nil
+    }
+    
+    // MARK: - methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func configureConstraints() {
+        self.contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    func bind(title: String) {
+        titleLabel.text = title
+    }
 }
