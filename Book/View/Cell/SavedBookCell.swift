1c9e7ca2ccfb4fcb7749ebe9367d3a83a84eeb51
KJH <gh7052@gmail.com>
2024-05-09 15:17:59 +0900

    Add: SavedBookCell 추가

---
+++ Book/View/Cell/SavedBookCell.swift
@@ -0,0 +1,17 @@
+//
+//  SavedBookCell.swift
+//  Book
+//
+//  Created by 김정호 on 5/9/24.
+//
+
+import UIKit
+
+final class SavedBookCell: UITableViewCell {
+    
+    // MARK: - properties
+    
+    // MARK: - life cycles
+    
+    // MARK: - methods
+}
