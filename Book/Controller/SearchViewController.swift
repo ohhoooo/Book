81d0898f56eb1a7dda073f53dd4313d87524b465
KJH <gh7052@gmail.com>
2024-05-10 05:41:25 +0900

    Feat: SearchViewController 최근 본 책 가져오기 기능 추가

--- Book/Controller/SearchViewController.swift
+++ Book/Controller/SearchViewController.swift
@@ -13,12 +13,14 @@ final class SearchViewController: UIViewController {
     private let searchView = SearchView()
     
     private let networkManager = NetworkManager.shared
+    private let userDefaultsManager = UserDefaultsRecentViewedBookStorage.shared
     
     private var query = ""
     private var sort = Sort.accuracy
     private var page = 1
     private var isEnd = false
     
+    private var recentViewedBooks: [String] = []
     private var searchResultBooks: [Book] = []
     
     // MARK: - life cycles
@@ -29,11 +31,16 @@ final class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureData()
         configureSearchBar()
         configureCollectionView()
     }
     
     // MARK: - methods
+    private func configureData() {
+        recentViewedBooks = userDefaultsManager.fetchRecentViewedBooks()
+    }
+    
     private func configureSearchBar() {
         self.searchView.searchBar.delegate = self
     }
@@ -42,6 +49,7 @@ final class SearchViewController: UIViewController {
         self.searchView.collectionView.delegate = self
         self.searchView.collectionView.dataSource = self
         
+        self.searchView.collectionView.register(RecentViewedBookCell.self, forCellWithReuseIdentifier: "RecentViewedBookCell")
         self.searchView.collectionView.register(SearchResultBookCell.self, forCellWithReuseIdentifier: "SearchResultBookCell")
         self.searchView.collectionView.register(SearchResultBookHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SearchResultBookHeaderView")
     }
@@ -70,25 +78,32 @@ final class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let text = searchBar.text, !text.isEmpty {
-            searchBar.resignFirstResponder()
-            self.query = text
+        if let query = searchBar.text, !query.isEmpty {
+            self.page = 1
+            self.query = query
+            self.isEnd = false
+            self.searchResultBooks = []
+            
             fetchSearchResultBooks()
+            searchBar.resignFirstResponder()
         }
     }
 }
 
 extension SearchViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if indexPath.section == 0 && indexPath.row == searchResultBooks.count - 1 && isEnd == false {
+        if indexPath.section == 1 && indexPath.row == searchResultBooks.count - 1 && isEnd == false {
             fetchSearchResultBooks()
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if indexPath.section == 0 {
+        if indexPath.section == 1 {
             let bookDetailVC = BookDetailViewController()
             bookDetailVC.bind(book: searchResultBooks[indexPath.row])
+            
+            recentViewedBooks = userDefaultsManager.saveRecentViewedBook(title: searchResultBooks[indexPath.row].title)
+            searchView.collectionView.reloadData()
             
             self.present(bookDetailVC, animated: true)
         }
@@ -103,7 +118,11 @@ extension SearchViewController: UICollectionViewDataSource {
                 return UICollectionReusableView()
             }
             
-            header.prepare(text: "검색 결과")
+            if indexPath.section == 0 {
+                header.prepare(text: "최근 본 책")
+            } else {
+                header.prepare(text: "검색 결과")
+            }
             
             return header
         default:
@@ -112,20 +131,34 @@ extension SearchViewController: UICollectionViewDataSource {
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return searchResultBooks.count
+        if section == 0 {
+            return recentViewedBooks.count
+        } else {
+            return searchResultBooks.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchResultBookCell", for: indexPath) as? SearchResultBookCell else {
-            return UICollectionViewCell()
+        if indexPath.section == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecentViewedBookCell", for: indexPath) as? RecentViewedBookCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.bind(title: recentViewedBooks[indexPath.row])
+            
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchResultBookCell", for: indexPath) as? SearchResultBookCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.bind(book: searchResultBooks[indexPath.row])
+            
+            return cell
         }
-        
-        cell.bind(book: searchResultBooks[indexPath.row])
-        
-        return cell
     }
 }
