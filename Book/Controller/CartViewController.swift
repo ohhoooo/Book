e1a2222d7ede99b9f3566bf78c15602e80bf6b5e
KJH <gh7052@gmail.com>
2024-05-09 17:10:22 +0900

    Feat: CartViewController trailingSwipeActionsConfigurationForRowAt 메소드 추가

--- Book/Controller/CartViewController.swift
+++ Book/Controller/CartViewController.swift
@@ -43,7 +43,54 @@ final class CartViewController: UIViewController {
 }
 
 extension CartViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteContextualAction = UIContextualAction(style: .normal, title: nil) { (_, _, success: @escaping (Bool) -> Void) in
+            let alertVC = UIAlertController(title: "정말로 삭제 하시겠습니까?", message: nil, preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+            let checkAction = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
+                guard let self = self else { return }
+                
+                coreDataStorage.deleteBook(book: books[indexPath.row]) { [weak self] result in
+                    guard let self = self else { return }
+                    
+                    switch result {
+                    case .success(let savedBooks):
+                        books = savedBooks
+                        tableView.deleteRows(at: [indexPath], with: .fade)
+                    case .failure(let error):
+                        switch error {
+                        case CoreDataError.notfoundItemError:
+                            printCheckAlert(title: "해당 책을 찾을 수 없습니다.")
+                        default:
+                            printCheckAlert(title: "알 수 없는 오류입니다.")
+                        }
+                    }
+                    
+                    success(true)
+                }
+            }
+            
+            alertVC.addAction(checkAction)
+            alertVC.addAction(cancelAction)
+            
+            self.present(alertVC, animated: true)
+        }
+        
+        deleteContextualAction.backgroundColor = .systemRed
+        deleteContextualAction.image = UIImage(systemName: "trash.fill")
+        
+        return UISwipeActionsConfiguration(actions: [deleteContextualAction])
+    }
     
+    func printCheckAlert(title: String) {
+        let alertVC = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let checkAction = UIAlertAction(title: "확인", style: .default)
+        
+        alertVC.addAction(checkAction)
+        
+        self.present(alertVC, animated: true)
+    }
 }
 
 extension CartViewController: UITableViewDataSource {
--- Book/Model/PersistentStorage/CoreData/CoreDataError.swift
+++ Book/Model/PersistentStorage/CoreData/CoreDataError.swift
@@ -9,4 +9,5 @@ import Foundation
 
 enum CoreDataError: Error {
     case existingItemError
+    case notfoundItemError
 }
