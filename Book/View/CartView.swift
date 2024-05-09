d7242a9675f77c1c336e8dc0274e5b3da13d7900
KJH <gh7052@gmail.com>
2024-05-09 15:14:34 +0900

    Design: CartView UI 구성

--- Book/View/CartView.swift
+++ Book/View/CartView.swift
@@ -6,16 +6,48 @@
 //
 
 import UIKit
+import SnapKit
 
 final class CartView: UIView {
     
     // MARK: - properties
+    let allBooksDeletionButton: UIButton = {
+        let button = UIButton()
+        let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 15, weight: .medium), .foregroundColor: UIColor.gray]
+        let attributedTitle = NSAttributedString(string: "전체 삭제", attributes: attributes)
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "담은 책"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 22, weight: .heavy)
+        return label
+    }()
+    
+    let bookAdditionButton: UIButton = {
+        let button = UIButton()
+        let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 15, weight: .medium), .foregroundColor: UIColor(red: 107/255, green: 184/255, blue: 100/255, alpha: 1)]
+        let attributedTitle = NSAttributedString(string: "추가", attributes: attributes)
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .none
+        return tableView
+    }()
     
     // MARK: - methods
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         configureUI()
+        configureConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -24,5 +56,33 @@ final class CartView: UIView {
     
     private func configureUI() {
         self.backgroundColor = .white
+    }
+    
+    private func configureConstraints() {
+        self.addSubview(allBooksDeletionButton)
+        self.addSubview(titleLabel)
+        self.addSubview(bookAdditionButton)
+        self.addSubview(tableView)
+        
+        allBooksDeletionButton.snp.makeConstraints {
+            $0.centerY.equalTo(self.titleLabel.snp.centerY)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+            $0.centerX.equalToSuperview()
+        }
+        
+        bookAdditionButton.snp.makeConstraints {
+            $0.centerY.equalTo(self.titleLabel.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(self.bookAdditionButton.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 }
